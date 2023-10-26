7b7ce4ceaf638d81239c28ad718897e884b5e0fe
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-10-26 22:54:53 +0800

    refactor(score-table): detach each table row
    this is to achieve multi-row editing

--- src/components/ScoreTable.vue
+++ src/components/ScoreTable.vue
@@ -1,156 +1,71 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { nanoid } from 'nanoid';
+import ScoreTableElement from './ScoreTableElement.vue';
 
-const { gradeList, selectedIndex } = defineProps({
+const { gradeList, editList } = defineProps({
 	gradeList: {
-		type: Array,
-		default: reactive([]),
+		type: Object,
+		default: () => ({}),
 	},
-	selectedIndex: {
-		type: Number,
-		default: ref(-1),
+	editList: {
+		type: Object,
+		default: () => ({}),
 	},
 });
-defineEmits(['rowSelect', 'addData']);
+const emit = defineEmits(['set-data', 'delete-data', 'toggle-edit']);
 
-const subjectValue = ref('');
-const levelValue = ref('s');
-const gradeValue = ref('a+');
-const creditValue = ref(0);
+function addData(subject, level, grade, credit) {
+	emit('set-data', nanoid(), subject, level, grade, credit);
+}
 
-const showNewItemRow = ref(false);
-
-function resetInput() {
-	subjectValue.value = '';
-	levelValue.value = 's';
-	gradeValue.value = 'a+';
-	creditValue.value = 0;
+function setData(id, subject, level, grade, credit) {
+	emit('set-data', id, subject, level, grade, credit);
 }
 </script>
 
 <template>
-	<table class="table">
-		<thead>
-			<tr>
-				<th>Subject</th>
-				<th>Level</th>
-				<th>Grade</th>
-				<th>Credits</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<template v-for="(item, index) in gradeList" :key="item.id">
-				<tr
-					:class="{ rowSelected: selectedIndex == index }"
-					@click="$emit('rowSelect', index)"
-				>
-					<td>{{ item.subject }}</td>
-					<td>{{ item.level }}</td>
-					<td>{{ item.grade }}</td>
-					<td>{{ item.credits }}</td>
-				</tr>
-			</template>
-			<tr v-if="gradeList.length == 0 || showNewItemRow">
-				<td>
-					<input
-						id="subject-input"
-						v-model="subjectValue"
-						type="text"
-						name="subject"
-						class="new-input"
-					/>
-				</td>
-				<td>
-					<select
-						id="level-input"
-						v-model="levelValue"
-						name="level"
-						class="new-select"
-					>
-						<option value="s">S</option>
-						<option value="h-">H-</option>
-						<option value="h">H</option>
-						<option value="ah">AH</option>
-						<option value="ap">AP</option>
-					</select>
-				</td>
-				<td>
-					<select
-						id="grade-input"
-						v-model="gradeValue"
-						name="grade"
-						class="new-select"
-					>
-						<option value="a+">A+</option>
-						<option value="a">A</option>
-						<option value="a-">A-</option>
-						<option value="b+">B+</option>
-						<option value="b">B</option>
-						<option value="b-">B-</option>
-						<option value="c">C</option>
-						<option value="d">D</option>
-						<option value="f">F</option>
-					</select>
-				</td>
-				<td>
-					<input
-						id="credit-input"
-						v-model="creditValue"
-						type="text"
-						name="credits"
-						class="new-input"
-					/>
-				</td>
-				<td>
-					<div class="flex space-x-2">
-						<button
-							class="group btn btn-outline btn-success btn-sm btn-square"
-							@click="
-								$emit(
-									'addData',
-									subjectValue,
-									levelValue,
-									gradeValue,
-									creditValue
-								)
+	<div class="max-w-[75%] overflow-y-auto flex">
+		<div>
+			<table class="table table-pin-rows w-full">
+				<colgroup>
+					<col class="w-fit whitespace-normal break-normal" />
+					<col span="2" />
+					<col class="w-fit" />
+					<col span="2" />
+				</colgroup>
+				<thead>
+					<tr>
+						<td>Subject</td>
+						<td>Level</td>
+						<td>Grade</td>
+						<td>Credits</td>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<template v-for="(item, id) in gradeList" :key="id">
+						<ScoreTableElement
+							:edit="editList[id]"
+							:subject="item.subject"
+							:level="item.level"
+							:grade="item.grade"
+							:credit="item.credits"
+							@delete-data="$emit('delete-data', id)"
+							@edit-data="$emit('toggle-edit', id)"
+							@confirm-data="
+								(subject, level, grade, credit) => {
+									emit('toggle-edit', id);
+									setData(id, subject, level, grade, credit);
+								}
 							"
-						>
-							<div
-								class="icon-[fluent--checkmark-12-regular] group-hover:icon-[fluent--checkmark-12-filled]"
-							></div>
-						</button>
-						<button
-							class="group btn btn-outline btn-error btn-sm btn-square"
-							@click="resetInput"
-						>
-							<div
-								class="icon-[fluent--dismiss-12-regular] group-hover:icon-[fluent--dismiss-12-filled]"
-							></div>
-						</button>
-					</div>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+						/>
+					</template>
+
+					<ScoreTableElement edit="true" @confirm-data="addData" />
+				</tbody>
+			</table>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-.new-input {
-	@apply input input-bordered;
-}
-.new-input:focus {
-	@apply input-primary;
-}
-
-.new-select {
-	@apply select select-bordered;
-}
-.new-select:focus {
-	@apply select-primary;
-}
-
-.row-select {
-	@apply bg-accent-focus text-accent-content;
-}
-</style>
+<style scoped></style>
---
+++ src/components/ScoreTableElement.vue
@@ -0,0 +1,177 @@
+<script setup>
+import { ref } from 'vue';
+
+const { subject, level, grade, credit, edit } = defineProps({
+	subject: {
+		type: String,
+		default: () => '',
+	},
+	level: {
+		type: String,
+		default: () => 'S',
+	},
+	grade: {
+		type: String,
+		default: () => 'A+',
+	},
+	credit: {
+		type: Number,
+		default: () => 0,
+	},
+	edit: {
+		type: Boolean,
+		default: () => false,
+	},
+});
+
+const emit = defineEmits([
+	'confirm-data',
+	'dismiss-data',
+	'edit-data',
+	'delete-data',
+]);
+
+const subjectValue = ref(subject);
+const levelValue = ref(level);
+const gradeValue = ref(grade);
+const creditValue = ref(credit);
+
+function dismissData() {
+	subjectValue.value = subject;
+	levelValue.value = level;
+	gradeValue.value = grade;
+	creditValue.value = credit;
+	emit('dismiss-data');
+}
+</script>
+
+<template>
+	<tr v-if="edit">
+		<td>
+			<input
+				id="subject-input"
+				v-model="subjectValue"
+				type="text"
+				name="subject"
+				class="new-input"
+			/>
+		</td>
+		<td>
+			<select
+				id="level-input"
+				v-model="levelValue"
+				name="level"
+				class="new-select"
+			>
+				<option value="S">S</option>
+				<option value="H-">H-</option>
+				<option value="H">H</option>
+				<option value="AH">AH</option>
+				<option value="AP">AP</option>
+			</select>
+		</td>
+		<td>
+			<select
+				id="grade-input"
+				v-model="gradeValue"
+				name="grade"
+				class="new-select"
+			>
+				<option value="A+">A+</option>
+				<option value="A">A</option>
+				<option value="A-">A-</option>
+				<option value="B+">B+</option>
+				<option value="B">B</option>
+				<option value="B-">B-</option>
+				<option value="C">C</option>
+				<option value="D">D</option>
+				<option value="F">F</option>
+			</select>
+		</td>
+		<td>
+			<input
+				id="credit-input"
+				v-model="creditValue"
+				type="text"
+				name="credits"
+				class="new-input"
+			/>
+		</td>
+		<th>
+			<div class="flex space-x-2">
+				<button
+					class="group btn btn-outline btn-success btn-sm btn-square"
+					@click="
+						$emit(
+							'confirm-data',
+							subjectValue,
+							levelValue,
+							gradeValue,
+							creditValue
+						)
+					"
+				>
+					<div
+						class="icon-[fluent--checkmark-12-regular] group-hover:icon-[fluent--checkmark-12-filled]"
+					></div>
+				</button>
+				<button
+					class="group btn btn-outline btn-error btn-sm btn-square"
+					@click="dismissData"
+				>
+					<div
+						class="icon-[fluent--dismiss-12-regular] group-hover:icon-[fluent--dismiss-12-filled]"
+					></div>
+				</button>
+			</div>
+		</th>
+	</tr>
+	<tr v-else>
+		<td>
+			<div class="break-normal">{{ subject }}</div>
+		</td>
+		<td>{{ level }}</td>
+		<td>{{ grade }}</td>
+		<td>{{ credit }}</td>
+		<th>
+			<div class="flex space-x-2">
+				<button
+					class="group btn btn-outline btn-info btn-sm btn-square"
+					@click="$emit('edit-data')"
+				>
+					<div
+						class="icon-[fluent--edit-12-regular] group-hover:icon-[fluent--edit-12-filled]"
+					></div>
+				</button>
+				<button
+					class="group btn btn-outline btn-error btn-sm btn-square"
+					@click="$emit('delete-data')"
+				>
+					<div
+						class="icon-[fluent--dismiss-12-regular] group-hover:icon-[fluent--dismiss-12-filled]"
+					></div>
+				</button>
+			</div>
+		</th>
+	</tr>
+</template>
+
+<style scoped>
+.new-input {
+	@apply input input-bordered;
+}
+.new-input:focus {
+	@apply input-primary;
+}
+
+.new-select {
+	@apply select select-bordered;
+}
+.new-select:focus {
+	@apply select-primary;
+}
+
+.row-select {
+	@apply bg-accent-focus text-accent-content;
+}
+</style>
